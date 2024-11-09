65fc7c52541735c6da0233fc9778d88e0b03ce6a
JarupongMin <129025834+mdeenit@users.noreply.github.com>
2024-11-09 10:21:41 +1000

    feat: init react app with a simple test

--- vite.config.js
+++ vite.config.js
@@ -8,6 +8,5 @@ export default defineConfig({
     globals: true, // use "describe", "expect", etc etc, without importing them in our test files
     environment: "jsdom", // makes our test work better as if they are in the browser 
     setupFiles: "src/setupTest.js" // do any test config before tests run. Runs once before ANY tests , not once before each test file. 
-  
   }
 })
